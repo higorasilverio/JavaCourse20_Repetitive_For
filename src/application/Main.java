3d4540f5f017e6b742faffcf3a658a225d55bc87
higorasilverio <higorasilverio@outlook.com>
2020-04-10 17:30:17 -0300

    Ponderate average between three numbers certain times - Final

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("*** Ponderate average between three numbers certain times ***");
+		System.out.println();
+
+		System.out.print("How many times we will verify? ");
+		int quantity = scanner.nextInt();
+		for (int i = 0; i < quantity; i++) {
+
+			double a = scanner.nextDouble();
+			double b = scanner.nextDouble();
+			double c = scanner.nextDouble();
+
+			double media = (a * 2.0 + b * 3.0 + c * 5.0) / 10.0;
+
+			System.out.printf("%.1f%n", media);
+		}
+
+		scanner.close();
+
+	}
+
+}
